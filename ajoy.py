3762d7562435f127a20ede7c0e802360c30918c3
dasyoja <71978315+dasyoja@users.noreply.github.com>
2020-09-27 21:10:26 +0530

    Add files via upload

---
+++ ajoy.py
@@ -0,0 +1,22 @@
+  
+print("content-type: text/html")
+print()
+import subprocess as sp
+import cgi
+form=cgi.FieldStorage()
+cmd=form.getvalue("x")
+
+if(cmd=="Open Notepad"):
+	sp.getoutput("notepad")
+elif(cmd=="Open Chrome"):
+	sp.getoutput("chrome")
+elif(cmd=="Open Firefox"):
+	sp.getoutput("firefox")
+elif(cmd=="virtualbox"):
+	sp.getoutput("virtualbox")
+elif(cmd=="Show Calendar"):
+	sp.getoutput("cal")
+elif(cmd=="Show Today's Date")
+	sp.getoutput("Date")
+else:
+	print(You entered wrong input\n")
